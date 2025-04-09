d3291cd80e0d9210f7b179d92eb313c4b47355d3
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-09 11:54:53 -0300

    feat: add Skeleton component for loading states
    feat: implement Tooltip component with Radix UI for enhanced UI tooltips
    
    feat: create useIsMobile hook to detect mobile devices
    
    chore: update yarn.lock with new dependencies for Radix UI components and utility libraries

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section className='flex flex-col md:flex-row'>
+      <div>menu lateral</div>
+      <article>Conteudo</article>
+    </section>
+  );
+}
