5392fe2652e3ea25dbadd0d52f583f30b7f0d4ae
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-09 11:04:17 -0300

    fix: change positioning of NavBar and Footer sections from fixed to absolute

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
     <html lang='pt-BR'>
       <body className={`${firaCode.className} text-foreground bg-[#020618]`}>
         <main className='bg-background m-4 h-[calc(100dvh-32px)] rounded-xl border'>
-          <section className='fixed top-4 left-0 z-10 w-full px-4'>
+          <section className='absolute top-4 left-0 z-10 w-full px-4'>
             <NavBar />
           </section>
           <section className='mt-14 h-[calc(100dvh-146px)]'>{children}</section>
-          <section className='fixed bottom-4 left-0 z-10 w-full px-4'>
+          <section className='absolute bottom-4 left-0 z-10 w-full px-4'>
             <Footer />
           </section>
         </main>
